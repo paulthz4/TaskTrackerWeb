54871cb6a62e90358ace871864230d3661831dae
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-04-21 15:33:51 -0600

    nodemon added

--- server/index.js
+++ server/index.js
@@ -2,16 +2,29 @@ const express  = require('express')
 const app = express()
 const {MongoClient} = require('mongodb')
 
-const uri = "mongodb+srv://casetrackerUser:<superSafe>@cluster0.uo7qm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const uri = "mongodb+srv://newUser2:superSafe2@cluster0.uo7qm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const client = new MongoClient(uri)
-
+let tasks = [];
 async function run(){
   try{
     await client.connect()
     
-    await client.db("case_tracker")
-    console.log("connected successfully to server")
+    const db = client.db("case_tracker")
+    //console.log("connected successfully to server")
+    const collection = db.collection("test_cases")
+    const query = { taskName: "api testing" }
+    
+    const options = {
+      sort:{projection: {_id:0, taskName: 1}}
+    }
+    
+    const task = collection.find({})
+    await task.forEach(doc => {
+      tasks.push(doc)
+      console.log(doc)
+    })
+    console.log(task)
   }
   finally{
     await client.close()
@@ -19,6 +32,15 @@ async function run(){
 }
 run().catch(console.dir)
 
+app.get('/', (req,res)=>{
+  run()
+  let str = "";
+  for(let i=0; i<tasks.length; i++){
+    str += +"\n"
+  }
+  res.send('hello world\n' + str)
+})
+
 app.listen(3002, () =>{
-  console.log("running on port 3001")
+  console.log("running on port 3002")
 })
