4a80dff2b223bdabdf76588dc7931063c6ca50f1
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-04-29 15:37:54 -0600

    route for searching task with 'taskName'

--- server/index.js
+++ server/index.js
@@ -1,30 +1,36 @@
-const express  = require('express')
-const app = express()
-const {MongoClient} = require('mongodb')
+const express  = require('express');
+const app = express();
+const {MongoClient} = require('mongodb');
 
-const uri = "mongodb+srv://newUser2:superSafe2@cluster0.uo7qm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.set('json spaces', 1);
 
-let collection
+const uri = "mongodb+srv://newUser2:superSafe2@cluster0.uo7qm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+let collection;
 // connecting to mongodb using MongoClient.connect static method
 MongoClient.connect(uri, (err, database) => {
   if (err) 
     return console.log(err)
-  collection = database.db('case_tracker').collection("test_cases")
+  collection = database.db('case_tracker').collection("cases")
   app.listen( 3002, () => {
   console.log('listening on 3002')
-  })
-})
+  });
+});
 
 app.get('/', (req,res)=>{
-  collection.find().project({_id: 0, taskName: 1, "date created": 1,"time created":1, "total time":1}).toArray((err,result)=>{
+  collection.find().sort({"date created":-1}).project({_id: 0}).toArray((err,result)=>{
     if(err)
-      console.log(err)
-    console.log(result)
-    res.jsonp(result)
-    console.log(res.body)
-
-  })
+      console.log(err);
+    // console.log(result);
+    res.status(200).json(result);
+  });
   
+  app.get('/tasks',(req,res)=>{
+    collection.find({"taskName": req.query.taskName.trim()}).sort({"date created": -1}).project({_id: 0})
+    .toArray((err,result)=>{
+      res.status(200).json(result);
+    })
+  });
  
-})
+});
 module.exports = app;
