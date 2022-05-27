10ade6cea82d536f354a1474f0eb92f4bf778d52
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-05-27 15:49:48 -0600

    added animation to task cards

--- client/Application-0/frontend/web.mql.todo/src/components/TaskItem.js
+++ client/Application-0/frontend/web.mql.todo/src/components/TaskItem.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import {ListItem, Box,  Typography} from "@mui/material";
+import {motion, AnimatePresence} from 'framer-motion/dist/framer-motion';
 
 export default function Task({task}){
-  
+  const variants = {
+    visible:{opacity: 1},
+    hidden: {opacity: 0}
+  }
+
   return(
-    <Box sx={{
-      boxShadow: 2,
-      my: 5,
-      p: 3,
-      borderRadius: 3,
-      backgroundColor: '#e6f4ff',
-      minWidth: 300,
-    }}>
+    <Box 
+      component={motion.div}
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      transition={{duration: 1.2}}
+      sx={{
+        boxShadow: 2,
+        my: 7,
+        p: 3,
+        borderRadius: 3,
+        backgroundColor: '#e6f4ff',
+        minWidth: 300,}}
+      >
       <ListItem >
         <Typography variant='h5'>{task.task_name}</Typography>
       </ListItem>
@@ -25,10 +36,10 @@ export default function Task({task}){
         <Typography><span style={{color: '#FFCA3C'}}>Total Time:</span> {task.total_time} </Typography>
       </ListItem>
       <ListItem>
-        <Typography><span style={{color: '#FFCA3C'}}> toppages:</span> {task.stoppages} </Typography>
+        <Typography><span style={{color: '#FFCA3C'}}> Stoppages:</span> {task.stoppages} </Typography>
       </ListItem>
       <ListItem>
-        <Typography><span style={{color: '#FFCA3C'}}>stoppage times:</span> {task.stoppage_times.map((time, index) => <><br/><span key={index}>{time}</span><br/></>)}</Typography>
+        <Typography><span style={{color: '#FFCA3C'}}>Stoppage times:</span> {task.stoppage_times.map((time, index) => <><br/><span key={index}>{time}</span><br/></>)}</Typography>
       </ListItem>
     </Box>
   );
