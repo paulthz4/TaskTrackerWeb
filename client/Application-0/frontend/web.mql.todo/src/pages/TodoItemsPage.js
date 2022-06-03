32b5b20aa897e77e77f13151288baae98b95ac38
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-06-03 14:08:44 -0600

    search bar bug fixed

--- client/Application-0/frontend/web.mql.todo/src/pages/TodoItemsPage.js
+++ client/Application-0/frontend/web.mql.todo/src/pages/TodoItemsPage.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState, useRef} from "react";
-import {  Container,Button,Typography,List,LinearProgress,Divider,} from "@material-ui/core";
+import React, {useEffect, useState} from "react";
+import {  Container,Button,Typography,List,LinearProgress,Divider, Box,} from "@material-ui/core";
 import '../components/App.css';
 import AddIcon from "@material-ui/icons/Add";
 import { useTodos } from "../hooks/useTodos";
@@ -8,31 +8,32 @@ import { useDraftTodos } from "../hooks/useDraftTodos";
 import { DraftTodoItem } from "../components/DraftTodoItem";
 import { useShowLoader } from "../hooks/util-hooks";
 import TaskItem from "../components/TaskItem";
-import {useTasks} from "../hooks/useTasks_Node";
 import axios from "axios";
 import { Autocomplete, TextField } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import {motion} from 'framer-motion/dist/framer-motion';
-import { flexbox } from "@mui/system";
+
 export function TodoItemsPage() {
   const { loading, todos, ...todoActions } = useTodos();
   // const { tasks }  = useTasks();
   const { draftTodos, ...draftTodoActions } = useDraftTodos();
   const showLoader = useShowLoader(loading, 200);
-  
   const [tasks, setTasks] = useState([]);
   const [searchText, setSearchText] = useState('');
-  let previousTasks = useRef([]);
+  const [options, setOptions] = useState([]);
   useEffect(()=>{
-     async function fetch(){axios.get('http://localhost:3002/').then(response => {
+     async function fetch(){await axios.get('http://localhost:3002/').then(response => {
       setTasks(response.data.tasks);
+      const array = [];
+      response.data.tasks.map(i => options.includes(i.task_name) ?  array : array.push(i.task_name));
+      setOptions(array);
        console.log(response);
      });
     }
     fetch();
+    
     console.log(tasks);
   },[]);
-  
   
   
   const onSearchClick= async()=>{
@@ -41,21 +42,11 @@ export function TodoItemsPage() {
     console.log(tasks)
   }
   
-  const handleClose =()=>{
-    setTasks(previousTasks.content);
-  }
-  
-  function handleInputChange(e,v){
-    //console.log(v);
-  }
-  
-  const options = [];
-  tasks.map(i => options.includes(i.task_name) ?  options : options.push(i.task_name));
   return (
     <Container component={motion.div} className="main-container"
       initial={{opacity:0}}
-      animate={{opacity:1}}
-      exit={{opacity:0, transition:{duration:0.5}}} 
+      animate={{opacity:1, transition:{duration:1.3}}}
+      exit={{opacity:0, transition:{duration:0.2}}} 
       maxWidth="sm">
       {loading ? (
         showLoader ? (
@@ -63,7 +54,6 @@ export function TodoItemsPage() {
         ) : null
       ) : (
         <div className="todo-items-container">
-        <motion.div initial={{opacity:0}} animate={{opacity:1, transition:{duration:3}}} style={{display:"flex", flexDirection:"column", justifyContent:"center",alignItems:"space-around"}} >
           <Typography component="p" variant="h5">
             {`You have ${tasks.length} Task${
               tasks.length === 1 ? "" : "s"
@@ -77,18 +67,19 @@ export function TodoItemsPage() {
           >
             Add Task
           </Button>
+          <div style={{display:"flex", alignItems:"center"}}>
           <Autocomplete
             inputValue={searchText}
             onInputChange={(e,v)=>setSearchText(v)}
             disablePortal
-            onClose={(e)=>handleClose}
-           // id="disable-close-on"
+            id="combo-box"
+            //onChange={(e,v)=>handleChange(e,v)}
             options={options}
-            sx={{width: "14em", height: "1em", margin: "2em"}}
+            sx={{ width:"15em", px:3}}
             renderInput={(params) => <TextField {...params} label="Search Task" />}
           />
           <SearchIcon onClick={()=>onSearchClick()} />
-          </motion.div>
+          </div>
           {draftTodos.map((draft) => (
               <DraftTodoItem
                 key={String(draft._id)}
