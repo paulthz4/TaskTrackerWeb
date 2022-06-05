aa12ca4ad0b084aedf010a5e1dea8aa7adc71258
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-06-04 21:45:23 -0600

    api tests

--- client/Application-0/backend/server/tests/app.test.js
+++ client/Application-0/backend/server/tests/app.test.js
@@ -8,11 +8,22 @@ describe("Test example", ()=>{
     // .expect("Content-Type", /json/)
     .expect(200)
     .expect((res)=>{
-      res.body.data.length = 21;
+      res.body.data.length = 22;
     })
     .end((err,res)=>{
       if(err) return done(err)
       return done()
     })
-  })
+  });
+  
+  test("GET tasks?",(done)=>{
+    request(app)
+    .get("/tasks?taskName=break")
+    .expect(200)
+    .expect("Content-Type", /json/)
+    .end((err,res)=>{
+      if(err) return done(err);
+      return done();
+    });
+  });
 })
