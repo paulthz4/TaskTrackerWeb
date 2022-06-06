4322538bdc04ce2b9f58b94838a61c53ef6275ae
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-06-06 10:44:36 -0600

    mapping data

--- client/Application-0/frontend/web.mql.todo/src/pages/Insights.js
+++ client/Application-0/frontend/web.mql.todo/src/pages/Insights.js
@@ -1,7 +1,8 @@
-import {React} from 'react';
-import { Line } from 'react-chartjs-2';
+import {React, useEffect, useState} from 'react';
+import { Bar, Line } from 'react-chartjs-2';
 import {Chart as ChartJS,CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Legend,} from 'chart.js';
 import {motion} from 'framer-motion/dist/framer-motion';
+import axios from 'axios';
 
 ChartJS.register(
   CategoryScale,
@@ -26,9 +27,9 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00'];
 
-export const data = {
+export const datas = {
   labels,
   datasets: [
     {
@@ -46,7 +47,22 @@ export const data = {
   ],
 };
 export default function Insights(){
-
+  const labels = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00'];
+  const [data, setData] = useState({
+    label: labels,
+    data: [1],
+    borderColor: 'rgb(255, 99, 132)',
+    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+  });
+  useEffect(()=>{
+    axios.get('http://localhost:3002/').then(response=>{
+      setData(values=>({
+        ...values, 
+        data: response.data.tasks.map(i=> i.stoppages)
+      }))
+    });
+    console.log(data)
+  },[]);
   return (
   <motion.div
     className="contact"
@@ -54,7 +70,7 @@ export default function Insights(){
     animate={{opacity:1, transition:{duration:1}}}
     exit={{opacity:0, transition:{duration:0.2}}}
   >
-    <Line options={options} data={data}/>
+    <Line  data={data}/>
   </motion.div> 
   );
 }
