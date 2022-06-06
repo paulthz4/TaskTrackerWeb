ac54429cb33ac6a0a369a4c4c56f0989800f6534
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-06-06 14:30:56 -0600

    chart shows stoppage data

--- client/Application-0/frontend/web.mql.todo/src/pages/Insights.js
+++ client/Application-0/frontend/web.mql.todo/src/pages/Insights.js
@@ -1,8 +1,9 @@
 import {React, useEffect, useState} from 'react';
-import { Bar, Line } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import {Chart as ChartJS,CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Legend,} from 'chart.js';
 import {motion} from 'framer-motion/dist/framer-motion';
 import axios from 'axios';
+import { Box } from '@material-ui/core';
 
 ChartJS.register(
   CategoryScale,
@@ -22,7 +23,7 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: 'Task Insights in Stoppage times',
     },
   },
 };
@@ -34,43 +35,53 @@ export const datas = {
   datasets: [
     {
       label: 'Dataset 1',
-      data: labels.map(() => 123),
+      data: [1,5,6,4,5,6,4],
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => 200),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
+   
   ],
 };
 export default function Insights(){
   const labels = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00'];
-  const [data, setData] = useState({
-    label: labels,
-    data: [1],
-    borderColor: 'rgb(255, 99, 132)',
-    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-  });
+  const [metaData, setMetaData] = useState([]);
+  const dataSet = {
+    labels,
+    datasets:[
+      {  
+        label: labels,
+        data: metaData,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ]
+  };
   useEffect(()=>{
     axios.get('http://localhost:3002/').then(response=>{
-      setData(values=>({
-        ...values, 
-        data: response.data.tasks.map(i=> i.stoppages)
-      }))
+      // setData(values=>({
+      //   ...values, 
+      //   data: response.data.tasks.map(i=> i.stoppages)
+      // }))
+      let arr = [];
+      response.data.tasks.map(i=>i.stoppage_times.map(j=>{
+          let temp = [];
+          temp = j.split(":");
+          arr.push(parseInt(temp[0]) + (temp[1]/60))
+        })); 
+      setMetaData(arr);
+      console.log(metaData);
+      console.log(arr);
     });
-    console.log(data)
+    
   },[]);
   return (
-  <motion.div
+  <Box component={motion.div} sx={{my:15}}
     className="contact"
     initial={{opacity:0}}
     animate={{opacity:1, transition:{duration:1}}}
     exit={{opacity:0, transition:{duration:0.2}}}
   >
-    <Line  data={data}/>
-  </motion.div> 
+    <Line options={options} data={dataSet}/>
+  </Box> 
   );
 }
