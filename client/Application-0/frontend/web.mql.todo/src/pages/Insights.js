88bd538024c498b5f88a3a2e63f1144a12b13001
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-06-21 16:10:41 -0600

    box plot data

--- client/Application-0/frontend/web.mql.todo/src/pages/Insights.js
+++ client/Application-0/frontend/web.mql.todo/src/pages/Insights.js
@@ -35,43 +35,17 @@ export const options1 = {
   },
 };
 
-export const options2 = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'right',
-    },
-    title: {
-      display: true,
-      text: 'Total Length of Tasks per Day',
-    },
-  },
-};
-
 export const timeLabels = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00'];
 
-export const example = {
-  timeLabels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: [1,5,6,4,5,6,4.5,6,8,5],
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-   
-  ],
-};
 export default function Insights(){
   const [labels, setLabels] = useState([]);
-  const [metaData, setMetaData] = useState([]);
+  const [stoppageData, setstoppageData] = useState([]);
   
   const [lengthData, setLengthData] = useState([]);
-  const [lengthLabels, setLengthLabels] = useState([]);
   
   // chart 2 polar chart
-  const [expectedData, setExpectedData] = useState([]);
-  const [actualData, setActualData] = useState([]);
+  const [polarData, setPolarData] = useState([]);
+  const [polarLabels, setPolarLabels] = useState([]);
   
   // box plot data
   const [boxplotData, setBoxPlotData] = useState([]);
@@ -86,19 +60,19 @@ export default function Insights(){
           arr.push(parseInt(temp[0]) + (temp[1]/60))
         })); 
       *****/
-      const stoppageMap = new Map();
+      const map = new Map();
       const lengthMap = new Map();
-      const expectData = new Map();
+      const polarMap = new Map();
       const boxplotMap = new Map();
       // loops through the 'tasks' array response from the axios request to make use of the data in the charts
        for(let i = 0; i < response.data.tasks.length; i++){
         // for the total number of stoppages chart. Adds the total number of stoppages in a day. Key => date_created, value=> number of stoppages
-        if(stoppageMap.has(response.data.tasks[i].date_created)){
-          stoppageMap.set(response.data.tasks[i].date_created, stoppageMap.get(response.data.tasks[i].date_created) + response.data.tasks[i].stoppages);
+        if(map.has(response.data.tasks[i].date_created)){
+          map.set(response.data.tasks[i].date_created, map.get(response.data.tasks[i].date_created) + response.data.tasks[i].stoppages);
           
         }
         else{
-          stoppageMap.set(response.data.tasks[i].date_created, response.data.tasks[i].stoppages);
+          map.set(response.data.tasks[i].date_created, response.data.tasks[i].stoppages);
         }
         // for the length of tasks. 
         let temp = response.data.tasks[i].total_time+"";
@@ -116,12 +90,11 @@ export default function Insights(){
           lengthMap.set(response.data.tasks[i].date_created, time);
           
         // box plot chart
-        //expectData.set(response.data.tasks[i].task_name, response );
         let sessionTime = 0;
         response.data.tasks[i].stoppage_times.map(j => {
           let temp  = j.split(":");
           sessionTime = parseFloat(temp[0]) + (parseFloat(temp[1])/60) + (parseFloat(temp[2])/3600);
-          console.log("ses time", sessionTime)
+          console.log("sess time", sessionTime)
        
           if(boxplotMap.has(response.data.tasks[i].task_name)){
             let arr = [];
@@ -134,10 +107,9 @@ export default function Insights(){
           }
         });
       }
-      setLabels(Array.from(stoppageMap.keys()));
-      setMetaData(Array.from(stoppageMap.values()));
+      setLabels(Array.from(map.keys()));
+      setstoppageData(Array.from(map.values()));
       
-      setLengthLabels(Array.from(lengthMap.keys()));
       setLengthData(Array.from(lengthMap.values()));
       
       console.log("length keys", Array.from(lengthMap.keys()));
@@ -149,19 +121,19 @@ export default function Insights(){
         let temp  = i.split(":");
         let sessionTime = 0;
         sessionTime = parseFloat(temp[0]) + (parseFloat(temp[1])/60) + (parseFloat(temp[2])/3600);
-        if(expectData.has(response.data.tasks[5].task_name)){
-          arr = Array.from(expectData.get(response.data.tasks[5].task_name));
+        if(polarMap.has(response.data.tasks[5].task_name)){
+          arr = Array.from(polarMap.get(response.data.tasks[5].task_name));
           console.log("arr", arr);
-          expectData.set(response.data.tasks[5].task_name, expectData.get(index) + sessionTime);
+          polarMap.set(response.data.tasks[5].task_name, polarMap.get(index) + sessionTime);
         }
         else
-        expectData.set(index + 1, sessionTime);
+        polarMap.set(index + 1, sessionTime);
      });
-     console.log(Array.from(expectData.keys()));
-     console.log(Array.from(expectData.values()));
+     console.log(Array.from(polarMap.keys()));
+     console.log(Array.from(polarMap.values()));
      
-     setExpectedData(Array.from(expectData.values()));
-     setActualData(Array.from(expectData.keys()));
+     setPolarData(Array.from(polarMap.values()));
+     setPolarLabels(Array.from(polarMap.keys()));
      
      setBoxPlotData(Array.from(boxplotMap.values()));
      setBoxplotLabels(Array.from(boxplotMap.keys()));
@@ -171,12 +143,12 @@ export default function Insights(){
     });
 
   },[]);
-  const stoppageData = {
+  const stoppageChart = {
     labels,
     datasets:[
       {  
         label: "Stoppages",
-        data: metaData,
+        data: stoppageData,
         borderColor: 'rgb(102, 102, 255)',
         backgroundColor: 'rgba(128, 128, 255, 0.5)',
       },
@@ -194,7 +166,7 @@ export default function Insights(){
     labels: boxplotLabels,
     datasets: [
       {
-        label: "Task length data",
+        label: "Task length",
         backgroundColor: "rgba(255,0,0,0.5)",
         borderColor: "red",
         borderWidth: 1,
@@ -206,11 +178,11 @@ export default function Insights(){
     ]
   };
   const chart3 = {
-    labels: actualData,
+    labels: polarLabels,
     datasets: [
     {
       label: '# of Votes',
-      data: expectedData,
+      data: polarData,
       backgroundColor: [
         'rgba(255, 99, 132, 0.5)',
         'rgba(54, 162, 235, 0.5)',
@@ -226,25 +198,39 @@ export default function Insights(){
   //console.log(datas)
   return (
   <Box component={motion.div} sx={{width:"60%"}}
-    className="chart"
+    className="chart-container"
     initial={{opacity:0}}
     animate={{opacity:1, transition:{duration:1}}}
     exit={{opacity:0, transition:{duration:0.2}}}
   >
-    <Line options={options1} data={stoppageData}/>
+    <Line options={{
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'right',
+              },
+              title: {
+                display: true,
+                text: 'Tasks Insights ',
+              },
+            },
+          }}
+     data={stoppageChart} className="chart"/>
     <Chart
         type="boxplot"
-        height={100}
-        legend={{
-          display: false
-        }}
         data={boxplotchart}
+        className="chart"
         options={{
           responsive: true,
-          title: {
-            display: true,
-            text: "Chart.js Box Plot Chart"
-          }
+          plugins: {
+            legend: {
+              position: 'right',
+            },
+            title: {
+              display: true,
+              text: 'Total Length of Tasks per Day',
+            },
+          },
         }}
       />
     <PolarArea data={chart3} />
