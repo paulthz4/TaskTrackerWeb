d9bb0cb1ad3093f73e03e391415277e6b56fdfbe
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-08-08 23:01:21 -0600

    tests api GET route

--- client/Application-0/backend/server/tests/app.test.js
+++ client/Application-0/backend/server/tests/app.test.js
@@ -3,7 +3,7 @@ const app = require("../index")
 
 describe("Test example", ()=>{
   test("GET /",(done)=>{
-    request(app)
+    const res = await request(app)
     .get("/") 
     // .expect("Content-Type", /json/)
     .expect(200)
@@ -13,7 +13,17 @@ describe("Test example", ()=>{
     .end((err,res)=>{
       if(err) return done(err)
       return done()
-    })
+    });
+    
+    expect(res.body.tasks[0]).toEqual({
+      _id: "62aa418a9014340022467091",
+      task_name: "task tracker",
+      date_created: "2022 06 15 Wed. Jun.",
+      time_created: "14:31:05",
+      total_time: "0",
+      stoppages: 0,
+      stoppage_times: []
+    });
   });
   
   test("GET tasks?",(done)=>{
@@ -21,6 +31,7 @@ describe("Test example", ()=>{
     .get("/tasks?taskName=break")
     .expect(200)
     .expect("Content-Type", /json/)
+    .expect(res.body)
     .end((err,res)=>{
       if(err) return done(err);
       return done();
