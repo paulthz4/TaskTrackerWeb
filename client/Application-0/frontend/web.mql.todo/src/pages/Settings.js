c1c9623f3ac9236c90b0161db9cfe4ccce5d8e1e
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-08-17 18:56:36 -0600

    settings table

--- client/Application-0/frontend/web.mql.todo/src/pages/Settings.js
+++ client/Application-0/frontend/web.mql.todo/src/pages/Settings.js
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
 import SettingsIcon from '@mui/icons-material/Settings';
-import {Box, Card} from "@mui/material";
+import {Box, Card, Typography,FormGroup, Switch, FormControlLabel, InputLabel, TableContainer, TableHead, TableBody, TableRow, TableCell, Table} from "@mui/material";
 import {motion} from 'framer-motion/dist/framer-motion';
-import { Typography } from '@material-ui/core';
 import './pages.css';
 
 export default function Settings(){
@@ -14,8 +13,27 @@ export default function Settings(){
       exit={{opacity:0, transition:{duration:0.2}}} 
       sx={{display: "flex", flexDirection:"column", width: "100%", margin:"5% auto", alignItems:"center", justifyContent:"center"}}
     >
-      <Card sx={{padding: "3em", width:"90%"}} className="tracker-container">
+      <Card elevation={0} sx={{padding: "3em", width:"80%", backgroundColor: '#e6f7ff'}} className="tracker-container">
         <Typography variant="h3" p={3}>Settings</Typography>
+        <TableContainer elevation={0} component={Card} sx={{width:"50%"}}>
+          <Table >
+            <TableHead></TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell >Dark Mode</TableCell>
+                <TableCell ><Switch /></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Email</TableCell>
+                <TableCell>test@test.com</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>tasks</TableCell>
+                <TableCell>42</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Card>
     </Box>
   );
